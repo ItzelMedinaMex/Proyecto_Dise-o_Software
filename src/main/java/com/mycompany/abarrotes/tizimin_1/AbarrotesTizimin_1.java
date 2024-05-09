433be6b09dc0fae67da5eda8f3df0552f3e21cdf
Itzel <BLANC-ITZ@10.0.144.172>
2024-05-09 12:21:48 -0500

    Subiendo todo, chavales.

---
+++ src/main/java/com/mycompany/abarrotes/tizimin_1/AbarrotesTizimin_1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.abarrotes.tizimin_1;
+
+import vista.Vista;
+
+/**
+ *
+ * @author BLANC-ITZ
+ */
+public class AbarrotesTizimin_1 {
+
+    public static void main(String[] args) {
+        Vista v= new Vista();
+        v.setVisible(true);
+    }
+}
